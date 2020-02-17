2e3214071cd77fd6971449a88376d20121f23e7f
Ramon Medeiros <ramon.medeiros@recordunion.com>
2020-02-17 21:28:59 +0100

    Save answer separated file

--- pizza/submission.go
+++ pizza/submission.go
@@ -1,62 +1,75 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const EXAMPLE = "a_example.in"
 const SMALL = "b_small.in"
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
-
 
 func parseDataset(filePath string) (int, int, []int) {
 
-    // open fd
-    fileDescriptor, err := os.Open(filePath)
-    check(err)
-    defer fileDescriptor.Close()
+	// open fd
+	fileDescriptor, err := os.Open(filePath)
+	check(err)
+	defer fileDescriptor.Close()
 
-    // create buffer for read words
-    scanner := bufio.NewScanner(fileDescriptor)
-    scanner.Split(bufio.ScanWords)
+	// create buffer for read words
+	scanner := bufio.NewScanner(fileDescriptor)
+	scanner.Split(bufio.ScanWords)
 
-    // read file
-    var numbers []int
-    for scanner.Scan() {
-	item, _ := strconv.Atoi(scanner.Text())
-	numbers = append(numbers, item)
-    }
-    fmt.Println(numbers)
-    return numbers[0], numbers[1], numbers[2:]
+	// read file
+	var numbers []int
+	for scanner.Scan() {
+		item, _ := strconv.Atoi(scanner.Text())
+		numbers = append(numbers, item)
+	}
+	return numbers[0], numbers[1], numbers[2:]
+}
+
+func saveAnswer(array []int, filename string) {
+	// create file
+	s := []string{filename, ".answer"}
+	saveFile := strings.Join(s, "")
+	f, err := os.Create(saveFile)
+	check(err)
+	defer f.Close()
+
+	t := strconv.Itoa(len(array)) + "\n"
+	f.WriteString(t)
+	for x := 0; x < len(array); x++ {
+		v := strconv.Itoa(array[x]) + " "
+		f.WriteString(v)
+	}
+	f.WriteString("\n")
+	fmt.Println("File saved at", saveFile)
 }
 
 func main() {
-    totalSlices, types, sizesPizza:= parseDataset(os.Args[1])
+	totalSlices, types, sizesPizza := parseDataset(os.Args[1])
 
-    // convert string list to int
-    var sizes []int
-
-    sum := 0
-    index := 0
-    item := 0
-    for index = types -1; index >= 0; index-- {
-        item = sizesPizza[index]
-	if (item <= (totalSlices - sum) ) {
-            sum = sum + item
-	    sizes = append(sizes, index)
-        }
-    }
-    fmt.Println("Sizes readed", len(sizesPizza))
-    fmt.Println("Sizes length", types)
-    fmt.Println("Max slices", totalSlices)
-    fmt.Println("Sum", sum)
+	var sizes []int
+	var index, item int
+	
+	sum := 0
+	for index = types - 1; index >= 0; index-- {
+		item = sizesPizza[index]
+		if item <= (totalSlices - sum) {
+			sum = sum + item
+			sizes = append(sizes, index)
+		}
+	}
+	fmt.Println("Max Slices:", totalSlices)
+	fmt.Println("Sum:", sum)
+	saveAnswer(sizes, os.Args[1])
 }
-
